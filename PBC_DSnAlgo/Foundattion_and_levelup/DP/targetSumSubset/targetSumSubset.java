d20027b2f75b7b03b1d92ca74b5e28a3af96f9f2
Rahul Prajapati <raarav717@gmail.com>
2020-06-22 02:43:18 +0530

    targetSumSubset

---
+++ PBC_DSnAlgo/Foundattion_and_levelup/DP/targetSumSubset/targetSumSubset.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class targetSumSubset {
+    public static boolean tss(int[] arr,int tar,int idx){
+        if(tar==0){
+            return true;
+        }
+        if(idx==arr.length || tar<0){
+            return false;
+        }
+        boolean ans = tss(arr,tar-arr[idx],idx+1) || tss(arr,tar,idx+1);
+        return ans;   
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+        int tar = scn.nextInt();
+        System.out.print(tss(arr,tar,0));        
+    }
+}
