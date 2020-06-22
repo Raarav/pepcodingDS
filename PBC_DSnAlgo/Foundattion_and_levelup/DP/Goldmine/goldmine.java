d26da7964cf147f4e2b89364dc23040b9cd9ca3a
Rahul Prajapati <raarav717@gmail.com>
2020-06-23 04:30:28 +0530

    goldmine

---
+++ PBC_DSnAlgo/Foundattion_and_levelup/DP/Goldmine/goldmine.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class goldmine {
+
+    public static int gmineHelper(int i,int[][] arr,int j){
+        if(j==arr[0].length){
+            return arr[i][j-1];    
+        }
+        if(i<0 || i>arr.length-1){
+            return 0;
+        }
+        int up = gmineHelper(i-1, arr,j+1);
+        int same = gmineHelper(i, arr,j+1);
+        int down = gmineHelper(i+1, arr,j+1);
+        int max=0;
+        max += Math.max(down,Math.max(up,same));
+        return arr[i][j] + max;
+    }
+
+    public static int gmine(int[][] arr){
+        int[] ansArr = new int[arr.length];
+        int max=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            ansArr[i] = gmineHelper(i,arr,0);
+            if(max<ansArr[i]){
+                max=ansArr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                arr[i][j]=scn.nextInt();
+            }
+        }    
+        System.out.print(gmine(arr));
+    }    
+}
