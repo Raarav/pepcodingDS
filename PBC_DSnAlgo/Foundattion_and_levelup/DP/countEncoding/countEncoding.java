4061ea00d3fb41e6e4ebcfbb1dbaa6aec2d7fea7
Rahul Prajapati <raarav717@gmail.com>
2020-06-26 04:23:05 +0530

    complete count encoding

--- PBC_DSnAlgo/Foundattion_and_levelup/DP/countEncoding/countEncoding.java
+++ PBC_DSnAlgo/Foundattion_and_levelup/DP/countEncoding/countEncoding.java
@@ -1,19 +1,26 @@
 import java.util.*;
 
 public class countEncoding {
-    public static void cE(String str,String ans){
-        if(str.length()==0 || str.length()==1) return 
-        cE(str.subtring(1),ans);
-        String stcheck = str.substring(0,2);
-        if(Integer.parseInt(stcheck)<27){
-            int n = Integer.parseInt(stcheck)+64;
-            char ch = (char)n;
-            cE(str.substring(2),ans+ch);
+    public static int cE(String str,String ans){
+        if(str.length()==0) return 1;
+        int count=0;
+        if(str.charAt(0)!='0'){
+            char chs = str.charAt(0);
+            count+=cE(str.substring(1),ans+chs);
         }
+        if(str.length()>1){
+            String stcheck = str.substring(0,2);        
+            if(Integer.parseInt(stcheck)<27 && str.length()>1){
+                int n = Integer.parseInt(stcheck)+64;
+                char ch = (char)n;
+                count += cE(str.substring(2),ans+ch);
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
-        cE(str,"");     
+        System.out.print(cE(str,""));     
     }    
 }
