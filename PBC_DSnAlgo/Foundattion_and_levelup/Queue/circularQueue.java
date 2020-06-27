2903d4386ce688177ab96a42662e71bcff1eeab0
Rahul Prajapati <raarav717@gmail.com>
2020-06-27 19:21:22 +0530

    displayOperation Implementaed

--- PBC_DSnAlgo/Foundattion_and_levelup/Queue/circularQueue.java
+++ PBC_DSnAlgo/Foundattion_and_levelup/Queue/circularQueue.java
@@ -1,31 +1,54 @@
 import java.util.*;
 public class circularQueue {
-    circularQueue front;
-    circularQueue rear;
+    int front;
+    int size;
     int[] data;
     public circularQueue(int size){
         this.data = new int[size];
-        this.front = -1;
-        this.rear = -1;
+        this.front = 0;
+        this.size = 0;
     }
 
     public void display(){
-
+        int idx = this.front;
+        int cnt = 0;
+        while(cnt<this.size){
+            System.out.print(data[idx] + ",");
+            cnt++;
+            idx = (idx + 1)%data.length;
+        }
+        System.out.println();
     }
 
-    public void add(){
-
+    public void add(int val){
+        if(size==data.length){
+            System.out.print("Stack OverFlow");    
+        }
+        int rear = (front+size)%data.length;
+        data[rear]=val;
+        size++;
     }
 
     public int size(){
-        return 0;
+        return this.size;
     }
 
     public int peek(){
-        return 0;
+        if(size==0){
+            System.out.print("Stack underflow");
+            return -1;
+        }
+        int rv = front;        
+        return rv;
     }
 
-    public void remove(){
-
+    public int remove(){
+        if(size==0){
+            System.out.print("Stack underflow");
+            return -1;
+        }
+        int re = this.data[this.front%data.length];
+        front = front + 1;
+        return re;
     }
 }
---
+++ PBC_DSnAlgo/Foundattion_and_levelup/Queue/circularQueueClient.java
@@ -0,0 +1,12 @@
+// import circularQueue;
+public class circularQueueClient {
+    public static void main(String[] args){
+        circularQueue q = new circularQueue(5);
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        q.display();
+    }             
+}
