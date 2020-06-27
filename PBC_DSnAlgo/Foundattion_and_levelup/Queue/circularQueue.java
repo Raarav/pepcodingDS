7b89a3bc71dc6f023d5ce9b9468a9cbab744f817
Rahul Prajapati <raarav717@gmail.com>
2020-06-27 16:16:27 +0530

    Queue

---
+++ PBC_DSnAlgo/Foundattion_and_levelup/Queue/circularQueue.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class circularQueue {
+    circularQueue front;
+    circularQueue rear;
+    int[] data;
+    public circularQueue(int size){
+        this.data = new int[size];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    public void display(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public int peek(){
+        return 0;
+    }
+
+    public void remove(){
+
+    }
+}
