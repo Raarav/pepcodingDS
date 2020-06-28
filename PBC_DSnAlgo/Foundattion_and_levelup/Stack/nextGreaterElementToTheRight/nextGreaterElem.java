36f260f088d1908f7f1020db94e6cf181b9f8860
Rahul Prajapati <raarav717@gmail.com>
2020-06-28 22:50:40 +0530

    incomplete NGE

--- PBC_DSnAlgo/Foundattion_and_levelup/Stack/nextGreaterElementToTheRight/nextGreaterElem.java
+++ PBC_DSnAlgo/Foundattion_and_levelup/Stack/nextGreaterElementToTheRight/nextGreaterElem.java
@@ -5,10 +5,20 @@ public class nextGreaterElem {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        int[] res = new int[n];
         Stack<Integer> st = new Stack<>();
         for(int i=0;i<n;i++){
-            if()            
+            if(arr[i]<st.peek()&&st.size()!=0){
+                res[st.pop()]=arr[i];
+            }else if(arr[i]<&&st.size()!=0){
+
+            }
+            else{
+                st.push(i);
+            }            
         }
     }    
 }
