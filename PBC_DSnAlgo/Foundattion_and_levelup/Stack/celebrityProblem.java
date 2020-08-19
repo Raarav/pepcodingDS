befc3cf13591f3f5236b2e50cd20e9c633a1384e
Rahul Prajapati <raarav717@gmail.com>
2020-08-19 18:33:55 +0530

    celebrityProblem

---
+++ PBC_DSnAlgo/Foundattion_and_levelup/Stack/celebrityProblem.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class celebrityProblem {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            String line = br.readLine();
+            for (int k = 0; k < n; k++) {
+                arr[j][k] = line.charAt(k) - '0';
+            }
+        }
+
+        findCelebrity(arr);
+
+    }
+
+    public static void findCelebrity(int[][] arr) {
+        // if a celebrity is there print it's index (not position), if there is not then
+        // print "none"
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            st.push(i);
+        }
+        while(st.size()!=1){
+            int j = st.pop();
+            int i = st.pop();
+            if(arr[i][j]==1){
+                st.push(j);
+            }else{
+                st.push(i);
+            }
+        }
+        int i = st.pop();
+        boolean flag = true;
+        for(int j=0;j<arr.length;j++){
+            if(i!=j&&arr[i][j]==1){
+                flag = false;
+                break;
+            }
+        }
+        if(flag==true){
+            for(int j=0;j<arr.length;j++){
+                if(i!=j&&arr[j][i]==0){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag==true){
+            System.out.print(i);
+        }else{
+            System.out.print("none");
+        }
+    }
+
+}
