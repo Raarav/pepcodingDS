ab83fac48a2a1d59e4bc42b595f6e58a3e7ab4ce
Rahul Prajapati <raarav717@gmail.com>
2020-08-21 14:36:57 +0530

    minimumStack

---
+++ PBC_DSnAlgo/Foundattion_and_levelup/Stack_n_Queue/minimumStack.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class MinStack {
+        Stack < Integer > allData;
+        Stack < Integer > minData;
+
+        public MinStack() {
+            allData = new Stack < > ();
+            minData = new Stack < > ();
+        }
+
+
+        int size() {
+            // write your code here  
+            return allData.size();
+        }
+
+        void push(int val) {
+            // write your code here
+            allData.push(val);
+            if (minData.empty() == true || minData.peek() >= val) {
+                minData.push(val);
+            }
+        }
+
+        int pop() {
+            // write your code here
+            int val  = -1;
+            if (allData.empty() == false) {
+                val = allData.pop();
+                if (minData.empty() == false && minData.peek() == val) {
+                    minData.pop();
+                }
+            }
+            return val;
+        }
+
+        int top() {
+            // write your code here
+            int val = -1;
+            if (allData.empty() == false) {
+                val = allData.peek();
+            }
+            return val;
+        }
+
+        int min() {
+            // write your code here
+            int minval = -1;
+            if (minData.empty() == false) {
+                minval = minData.peek();
+            }
+            return minval;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        MinStack st = new MinStack();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("push")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if (str.startsWith("pop")) {
+                int val = st.pop();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("top")) {
+                int val = st.top();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(st.size());
+            } else if (str.startsWith("min")) {
+                int val = st.min();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            }
+            str = br.readLine();
+        }
+    }
+}
