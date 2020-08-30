1adb33e2156d7efb3f8854ba6aa8e9d2a59933d6
Rahul Prajapati <raarav717@gmail.com>
2020-08-31 02:02:51 +0530

    lcqs

--- PBC_DSnAlgo/Foundattion_and_levelup/HashMap/longestConsucativeSubsqence.java
+++ PBC_DSnAlgo/Foundattion_and_levelup/HashMap/longestConsucativeSubsqence.java
@@ -1,56 +1,53 @@
-import java.util.*;
 import java.io.*;
-import java.util.Map;
+import java.util.*;
 
-class longestConsucativeSubsqence{
+public class longestConsucativeSubsqence {
 
-    public static void lcsInArray(int[] arr) {
+    public static ArrayList<Integer> lcqs(int[] arr) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(hm.containsKey(arr[i])==false){
+        hm.put(arr[0],1);
+        for(int i=1;i<arr.length;i++){
+            if(hm.get(arr[i])==null){
                 hm.put(arr[i],1);
+            } 
+        }
+        for(Integer i : hm.keySet()){
+            if(hm.containsKey(i-1)==false){
+                int val = hm.get(i);
+                int ival=i+1;
+                while(hm.containsKey(ival)){
+                    val+=1;
+                    ival+=1;
+                }
+                hm.put(i,val);
             }
         }
-        //ketSet()
-        for(int ele: hm.keySet()){
-            if(hm.containsKey(ele-1)==false && hm.containsKey(ele+1)==true){
-                ele=ele+1;
-                int ans = hm.get(ele);
-                while(hm.containsKey(ele)!=false){                    
-                    hm.put(ele,ans+1);
-                    System.out.println(ele + "->" + hm.get(ele));
-                    ele=ele+1;
-                }    
-            }
-        }
-        System.out.println(hm.keySet());
+        ArrayList<Integer> res = new ArrayList<>();
         int max = 0;
-        int maxele = 0;
-        for(int e : hm.keySet()){
-            if(max<hm.get(e)){
-                maxele=e;
-                max=hm.get(e);
+        int ele = 0;
+        for(Integer i : hm.keySet()){
+            if(max<hm.get(i)){
+                ele=i;
+                max=hm.get(i);
             }
         }
-        System.out.println("ooooooooooooooooooooo");
-        System.out.println(maxele);
-        System.out.println(max);
-        System.out.println("ooooooooooooooooooooo");
-        for(int i=0;i<max;i++){
-            System.out.println(maxele-i);   
+        for(int j = 0;j<max;j++){
+            res.add(ele + j);
         }
-
-        // System.out.print(hm); 
-        // System.out.print(hm.keySet());     
+        return res;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        // write your code here
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=scn.nextInt();
-        }    
-        lcsInArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        ArrayList<Integer> ans = lcqs(arr);
+        for (Integer i : ans) {
+            System.out.println(i);
+        }
     }
 }
