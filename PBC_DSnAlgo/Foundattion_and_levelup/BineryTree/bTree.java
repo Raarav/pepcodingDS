7f0f8119e0e445f123a7a1fb5961338791f11dd0
Rahul Prajapati <raarav717@gmail.com>
2020-09-29 16:55:07 +0530

    Print Single Child Nodes

--- PBC_DSnAlgo/Foundattion_and_levelup/BineryTree/bTree.java
+++ PBC_DSnAlgo/Foundattion_and_levelup/BineryTree/bTree.java
@@ -134,6 +134,129 @@ class bTree{
         System.out.print(node.data + " ");
     }
 
+    // print the node to root path
+    static ArrayList<Integer> path;
+    public static boolean node2rootpath(Node node, int val){
+        if(node == null) return false;  
+        
+        if(node.data == val){
+            path.add(node.data);
+            return true;
+        }
+
+        boolean leftAns = node2rootpath(node.left, val);
+        if(leftAns) {
+            path.add(node.data);
+            return true;
+        }
+        
+        boolean rightAns = node2rootpath(node.right, val);
+        if(rightAns) {
+            path.add(node.data);
+            return true;
+        }
+
+        return false;
+    }
+
+    // print k level down nodes
+    public static void pkld(Node node,int k){
+        if(node == null || k<0) return;
+
+        if(k==0) System.out.println(node.data + " ");
+
+        pkld(node.left, k-1);
+        pkld(node.right, k-1);
+    }
+
+    // print nodes k distance away
+    public static void pnKdistanceAway(Node node,int val,int k) {
+        root2nodePath = new ArrayList<>();
+        root2nodeFunc(node,val);   
+        for(int i = 0;i < root2nodePath.size(); i++){
+            kNodeDown(root2nodePath.get(i),k-i,i==0?null:root2nodePath.get(i-1));
+        } 
+    }
+
+    static ArrayList<Node> root2nodePath;
+    public static boolean root2nodeFunc(Node node, int val){
+        if(node == null) return false;
+
+        if(node.data == val){
+            root2nodePath.add(node);
+            return true;
+        }
+
+        boolean lft = root2nodeFunc(node.left,val);
+        if(lft){
+            root2nodePath.add(node);
+            return true;
+        }
+
+        boolean rft = root2nodeFunc(node.right,val);
+        if(rft){
+            root2nodePath.add(node);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void kNodeDown(Node node, int k, Node blocker){
+        if(node == null || k<0 || node == blocker) return;
+
+        if(k==0) System.out.print(node.data + " ");
+
+        kNodeDown(node.left,k-1,blocker);
+        kNodeDown(node.right,k-1,blocker);
+    }
+
+    public static void path2_LR_range(Node node,int lo,int hi, String path, int sum) {
+
+        if(node == null) return;
+
+        if(node.left == null && node.right == null) {
+            sum += node.data;
+            if(sum<=hi && sum>=lo){
+                System.out.println(path + node.data);
+            }
+            return;
+        }   
+        path2_LR_range(node.left,lo,hi,path + node.data + " ",sum + node.data);
+        path2_LR_range(node.right,lo,hi,path + node.data + " ",sum + node.data);     
+    }
+
+    // Transform To Left-cloned Tree
+    public static Node trans2_LC_tree(Node node) { 
+        if(node == null) return null;
+
+        Node lcr = trans2_LC_tree(node.left);
+        Node rcr = trans2_LC_tree(node.right);
+
+        Node newNode = new Node(node.data,lcr,null);
+        node.left = newNode;
+        node.right = rcr;
+        
+        return node;
+    } 
+
+    // Transform To Normal From Left-cloned Tree
+    public static Node transBack_normal_tree(Node node){
+        if(node == null) return null;
+
+        Node lf = transBack_normal_tree(node.left.left);
+        Node rt = transBack_normal_tree(node.right);
+        
+        node.left = lf;
+        node.right = rt;
+        
+        return node;
+        
+    }
+
+    // Print Single Child Nodes
+    print_singleChild(root)
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
         int n = Integer.parseInt(br.readLine());
@@ -146,6 +269,12 @@ class bTree{
                 arr[i] = null;
             }
         } 
+
+        // int val = Integer.parseInt(br.readLine());
+        // int away = Integer.parseInt(br.readLine());
+        
+        // int lo = Integer.parseInt(br.readLine());
+        // int hi = Integer.parseInt(br.readLine());
         
         Node root = new Node(arr[0],null,null);
         stackPair p = new stackPair(root,1);
@@ -201,9 +330,36 @@ class bTree{
         // lot(root); 
 
         //print preorder inorder postorder
-        pipOrders(root);
+        // pipOrders(root);
+
+        // node to root path
+        // path = new ArrayList<>();
+        // System.out.println(node2rootpath(root,val));
+        // System.out.println(path); 
+        
+        // print k level down nodes
+        // pkld(root, val);
+
+        // print nodes k distance away
+        // root2nodePath = new ArrayList<>();
+        // pnKdistanceAway(root,val,away);
+
+        // Path To Leaf From Root In Range
+        // path2_LR_range(root,lo,hi,"",0);
+
+        // Transform To Left-cloned Tree
+        // trans2_LC_tree(root);
+        
+        // Transform To Normal From Left-cloned Tree
+        // display(transBack_normal_tree(root));
+
+        // Print Single Child Nodes
+        print_singleChild(root);
+
+
     }
 }
 
 // 19
 // 50 25 12 n n 37 30 n n n 75 62 n 70 n n 87 n n
+// 30 
