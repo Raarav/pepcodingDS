f5bfda28040e951e64f173d98e869f9dc4e89ec0
Rahul Prajapati <raarav717@gmail.com>
2020-10-16 02:47:19 +0530

    Q301

--- PBC_DSnAlgo/IP/genericTree/Serialize and Deserialize an N-ary Tree/Q301.java
+++ PBC_DSnAlgo/IP/genericTree/Serialize and Deserialize an N-ary Tree/Q301.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 class Q301 {
     static class Node{
-        String data;
+        char data;
         ArrayList<Node> children = new ArrayList<>();
     }
     
@@ -26,8 +26,8 @@ class Q301 {
     }
     
     
-    public static Node construct(Node root,String[] arr){
-        HashMap<String,Node> hm = new HashMap<>();
+    public static Node construct(Node root,char[] arr){
+        HashMap<Character,Node> hm = new HashMap<>();
         root.data = arr[0];
         hm.put(arr[0],root);
         for(int i=1;i<arr.length;i++){
@@ -43,20 +43,37 @@ class Q301 {
     }
 
     public static String serializer(Node node){
-        String str = node.data;
+        String str = node.data+"";
         for(Node child : node.children){
             str += serializer(child);
         }
         return str + ")";
     }
 
-    public static Node deserializer(){
-        
+    public static Node deserializer(String str){
+        Stack<Node> st = new Stack<>();
+        Node root = new Node();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch == ')'){
+                st.pop();                
+            } else {
+                Node temp = new Node();
+                temp.data = ch;
+                if(st.size()>0){
+                    st.peek().children.add(temp);                   
+                }else{
+                    root = temp;                    
+                }
+                st.push(temp);
+            }
+        }   
+        return root;     
     }
 
     public static void serializerDeserializer(Node root){
         String str = serializer(root);
-        System.out.print(str);
+        System.out.println(str);
         Node after_ser_root = deserializer(str);
         display(after_ser_root);    
     }
@@ -68,9 +85,11 @@ class Q301 {
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine());
             String[] str = br.readLine().split(" ");
-            String[] arr = new String[n];
+            // char[] charsFromString = str.toCharArray();
+            char[] arr = new char[n];
             for(int i=0;i<arr.length;i++){
-                arr[i] = str[i];
+                char c = str[i].charAt(0);
+                arr[i] = c;
             }
 
             Node root = new Node();
@@ -82,6 +101,6 @@ class Q301 {
 }
 
 // input:-
-1
-20
-A B A C A D B E B F F K D G D H D I D J
+// 1
+// 20
+// A B A C A D B E B F F K D G D H D I D J
